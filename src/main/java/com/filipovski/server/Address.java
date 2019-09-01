af8f45a10bbdea7c53514796f56aa781aa8ddbc4
Darko Filipovski <filipovskidarko>
2019-09-01 13:49:01 +0200

    Bad approach working http proxy

---
+++ src/main/java/com/filipovski/server/Address.java
@@ -0,0 +1,27 @@
+package com.filipovski.server;
+
+public class Address {
+	private String host;
+	private int port;
+	
+	public Address(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+}
