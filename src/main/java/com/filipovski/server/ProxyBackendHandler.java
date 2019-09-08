cc04bcdc7faa0822034faf038d8ece61f5b56da5
Darko Filipovski <darkofilipovski0@gmail.com>
2019-09-08 10:11:20 +0200

    Working http proxy

--- src/main/java/com/filipovski/server/ProxyBackendHandler.java
+++ src/main/java/com/filipovski/server/ProxyBackendHandler.java
@@ -5,6 +5,7 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
+import io.netty.channel.PendingWriteQueue;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpClientCodec;
@@ -18,122 +19,94 @@ import java.util.Deque;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class ProxyBackendHandler extends SimpleChannelInboundHandler<HttpObject> {
-	private Connection connectionInfo;
-    private Channel outboundChannel;
-
-    private DelayOutboundHandler delayOutboundHandler;
-
+	private Connection connection;
+    private Channel inboundChannel;
+    private RequestOutboundHandler requestOutboundHandler;
+    private int actionNum;
+    
     private volatile HttpRequest currentRequest;
 
-    public ProxyBackendHandler(Connection connectionInfo, Channel outboundChannel) {
-        this.connectionInfo = connectionInfo;
-        this.outboundChannel = outboundChannel;
-
-        delayOutboundHandler = new DelayOutboundHandler();
+    public ProxyBackendHandler(Connection connection, Channel inboundChannel) {
+        this.connection = connection;
+        this.inboundChannel = inboundChannel;
+        this.requestOutboundHandler = new RequestOutboundHandler();
+        
+        this.actionNum = 0;
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        delayOutboundHandler.next();
+    	actionNum++;
+    	System.out.println("channelActive " + actionNum);
+        requestOutboundHandler.tryWritingRequests("ActiveOut");
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-//        LOGGER.info("{} : channelInactive", connectionInfo);
-        delayOutboundHandler.release();
+    	actionNum++;
+    	System.out.println("channelInactive " + actionNum);
+//        delayOutboundHandler.release();
 //        outboundChannel.pipeline().fireUserEventTriggered(new OutboundChannelClosedEvent(connectionInfo, false));
     }
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-
+    	actionNum++;
+    	System.out.println("handlerAdded " + actionNum);
         ctx.pipeline()
            .addBefore(ctx.name(), null, new HttpClientCodec())
-           .addBefore(ctx.name(), null, delayOutboundHandler);
-        
-        System.out.println("Pipeline " + ctx.pipeline());
+           .addBefore(ctx.name(), null, this.requestOutboundHandler);
     }
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject)
             throws Exception {
-		System.out.println("Backend read");
+    	actionNum++;
+    	System.out.println("channelRead " + actionNum);
 
-//        LOGGER.info("[Client ({})] <= [Server ({})] : {}",
-//                    connectionInfo.getClientAddr(), connectionInfo.getServerAddr(),
-//                    httpObject);
-        outboundChannel.writeAndFlush(ReferenceCountUtil.retain(httpObject));
+    	inboundChannel.writeAndFlush(ReferenceCountUtil.retain(httpObject));
 
-        if (httpObject instanceof HttpResponse) {
-            currentRequest = null;
-            delayOutboundHandler.next();
-        }
+//        if (httpObject instanceof HttpResponse) {
+//            currentRequest = null;
+//            delayOutboundHandler.next();
+//        }
     }
 
-    private class DelayOutboundHandler extends ChannelOutboundHandlerAdapter {
-        private Deque<RequestPromise> pendings = new ConcurrentLinkedDeque<>();
-        private ChannelHandlerContext thisCtx;
+    public class RequestOutboundHandler extends ChannelOutboundHandlerAdapter {
+    	private PendingWriteQueue requestsQueue;
+    	private ChannelHandlerContext handlerContext;
+    	
+    	public synchronized void tryWritingRequests(String where) {
+    		System.out.println("	Try writing " + where);
 
-        @Override
+    		if(!handlerContext.channel().isActive() || this.requestsQueue.isEmpty()) {
+    			System.out.println("RETURN");
+    			return;
+    		}
+
+    		if(this.requestsQueue.removeAndWriteAll() != null)
+    			handlerContext.flush();
+    	}
+    	
+    	@Override
         public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-            thisCtx = ctx.pipeline().context(this);
-            System.out.println("thisCtx " + thisCtx);
+        	actionNum++;
+        	System.out.println("D: handlerAdded " + actionNum);
+            handlerContext = ctx.pipeline().context(this);
+    		requestsQueue = new PendingWriteQueue(ctx);
         }
-
-        @Override
-        public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-            if (msg instanceof FullHttpRequest) {
-//                LOGGER.info("[Client ({})] => [Server ({})] : (PENDING) {}",
-//                            connectionInfo.getClientAddr(), connectionInfo.getServerAddr(),
-//                            msg);
-        		System.out.println("Delay write");
-
-            	HttpRequest request = (HttpRequest) msg;
-                pendings.offer(new RequestPromise(request, promise));
-                next();
-            } else if (msg instanceof HttpObject) {
-                throw new IllegalStateException("Cannot handled message: " + msg.getClass());
-            } else {
-                ctx.write(msg, promise);
-            }
-        }
-
-        private void next() {
-        	System.out.println("CR " + currentRequest);
-        	System.out.println("Active " + thisCtx.channel().isActive());
-        	System.out.println("Pendings " + pendings.isEmpty());
-            if (currentRequest != null || !thisCtx.channel().isActive() || pendings.isEmpty()) {
-                System.out.println("RETURN");
-                return;
-            }
-
-            RequestPromise requestPromise = pendings.poll();
-            currentRequest = requestPromise.request;
-//            LOGGER.info("[Client ({})] => [Server ({})] : {}",
-//                        connectionInfo.getClientAddr(), connectionInfo.getServerAddr(),
-//                        requestPromise.request);
-
-            thisCtx.writeAndFlush(requestPromise.request, requestPromise.promise);
-            System.out.println("FLUSH");
-        }
-
-        private void release() {
-            while (!pendings.isEmpty()) {
-                RequestPromise requestPromise = pendings.poll();
-//                LOGGER.info("{} : {} is dropped", connectionInfo.toString(true), requestPromise.request);
-                requestPromise.promise.setFailure(new IOException("Cannot send request to server"));
-                ReferenceCountUtil.release(requestPromise.request);
-            }
-        }
-    }
-
-    private static class RequestPromise {
-        private HttpRequest request;
-        private ChannelPromise promise;
-
-        private RequestPromise(HttpRequest request, ChannelPromise promise) {
-            this.request = request;
-            this.promise = promise;
-        }
+    	
+    	@Override
+		public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
+        	actionNum++;
+        	System.out.println("D: write " + actionNum);
+        	
+    		if(msg instanceof FullHttpRequest) {
+    			HttpRequest request = (HttpRequest) msg;
+    			System.out.println("D: \n" + request);
+    			requestsQueue.add(request, promise);
+    			tryWritingRequests("In");
+    		}
+    	}
     }
 }
--- src/main/java/com/filipovski/server/ProxyFrontendHandler.java
+++ src/main/java/com/filipovski/server/ProxyFrontendHandler.java
@@ -1,11 +1,16 @@
 package com.filipovski.server;
 
+import java.net.UnknownHostException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -15,9 +20,8 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 
 public class ProxyFrontendHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
-	private static final Pattern PATH_PATTERN = Pattern.compile("(https?)://([a-zA-Z0-9\\.\\-]+)(:(\\d+))?(/.*)");
-	private static final Pattern TUNNEL_ADDR_PATTERN = Pattern.compile("^([a-zA-Z0-9\\.\\-_]+):(\\d+)");
-
+	private static final Pattern URI_PATTERN = Pattern.compile("(https?)://([a-zA-Z0-9\\.\\-]+)(:(\\d+))?(/.*)");
+	
 	private Connection connection;
 	private Channel outboundChannel;
 	private HttpServerCodec httpServerCodec;
@@ -30,15 +34,18 @@ public class ProxyFrontendHandler extends SimpleChannelInboundHandler<FullHttpRe
 
 	@Override
 	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-
 		httpServerCodec = new HttpServerCodec();
 		httpObjectAggregator = new HttpObjectAggregator(1024 * 1024);
-		ctx.pipeline().addBefore(ctx.name(), null, httpServerCodec).addBefore(ctx.name(), null, httpObjectAggregator);
+		ctx.pipeline()
+			.addBefore(ctx.name(), null, httpServerCodec)
+			.addBefore(ctx.name(), null, httpObjectAggregator);
 	}
 
 	@Override
 	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-		ctx.pipeline().remove(httpServerCodec).remove(httpObjectAggregator);
+		ctx.pipeline()
+			.remove(httpServerCodec)
+			.remove(httpObjectAggregator);
 
 		if (outboundChannel != null) {
 			outboundChannel.close();
@@ -48,62 +55,58 @@ public class ProxyFrontendHandler extends SimpleChannelInboundHandler<FullHttpRe
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
-		System.out.println("Frontend read");
 		if (request.method() == HttpMethod.CONNECT) {
+			System.out.println("CONNECT");
 			return;
 		} else {
+			System.out.println("HTTPS");
 			handleHttpRequest(ctx, request);
 		}
 	}
 
-	private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest request) {
-		FullPath fullPath = resolveHttpProxyPath(request.uri());
-		Address serverAddress = new Address(fullPath.host, fullPath.port);
-
-		if (outboundChannel != null && !connection.getServerAddress().equals(serverAddress)) {
-			outboundChannel.close();
-			outboundChannel = null;
-		}
-		if (outboundChannel != null && !outboundChannel.isActive()) {
-			outboundChannel.close();
-			outboundChannel = null;
-		}
-
-		if (outboundChannel == null) {
-			outboundChannel = createOutboundChannel(ctx, serverAddress);
-		}
-
+	private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest request) throws UnknownHostException {
+		ResolvedUri resolvedUri = resolveHttpUri(request.uri());
+		Address serverAddress = new Address(resolvedUri.hostname, resolvedUri.port);
+		outboundChannel = createOutboundChannel(ctx, serverAddress);
+		
 		FullHttpRequest newRequest = request.copy();
 		newRequest.headers().set(request.headers());
-		newRequest.setUri(fullPath.path);
+		newRequest.setUri(request.uri());
 
+		System.out.println("About to call write");
 		outboundChannel.writeAndFlush(newRequest);
 		System.out.println("Channel active " + outboundChannel.isActive());
 		System.out.println("Frontend written");
 	}
-
-	private FullPath resolveHttpProxyPath(String fullPath) {
-		Matcher matcher = PATH_PATTERN.matcher(fullPath);
-		if (matcher.find()) {
+	
+	private ResolvedUri resolveHttpUri(String uri) throws UnknownHostException {
+		Matcher matcher = URI_PATTERN.matcher(uri);
+		
+		if(matcher.find()) {
 			String scheme = matcher.group(1);
-			String host = matcher.group(2);
-			int port = resolvePort(scheme, matcher.group(4));
-			String path = matcher.group(5);
-			return new FullPath(scheme, host, port, path);
-		} else {
-			throw new IllegalStateException("Illegal http proxy path: " + fullPath);
+	        String hostname = matcher.group(2);
+	        int port = resolvePort(scheme, matcher.group(4));
+	        String path = matcher.group(5);
+	        return new ResolvedUri(scheme, hostname, port, path);
+		}
+		else {
+			throw new UnknownHostException("Could not resolve the http uri !!");		
 		}
 	}
-
-	private int resolvePort(String scheme, String port) {
-		if (port == null || port.isEmpty()) {
-			return "https".equals(scheme) ? 443 : 80;
+	
+	private int resolvePort(String scheme,String port) {
+		if(port == null || port.isEmpty()) { 
+			return scheme.equals("https") ? 443 : 80;
 		}
+		
 		return Integer.parseInt(port);
 	}
-
+	
 	private Channel createOutboundChannel(ChannelHandlerContext ctx, Address serverAddress) {
-		connection = new Connection(connection.getClientAddress(), new Address(serverAddress.getHost(), serverAddress.getPort()));
+		connection = new Connection(connection.getClientAddress(), 
+									new Address(serverAddress.getHost(), 
+												serverAddress.getPort()));
+		
 		Bootstrap bootstrap = new Bootstrap();
 		ChannelFuture future = bootstrap.group(ctx.channel()
 				.eventLoop())
@@ -115,31 +118,34 @@ public class ProxyFrontendHandler extends SimpleChannelInboundHandler<FullHttpRe
 		                ch.pipeline().addLast(new ProxyBackendHandler(connection, ctx.channel()));
 		            }
 		        })
-				.connect("50.62.232.1", 80);
-
-//		future.addListener(ChannelFutureListener.CLOSE);
-		System.out.println("ACTIVE: " + future.channel().isActive());
+				.connect(serverAddress.getHost(), serverAddress.getPort());
+		System.out.print("Server addr:" + serverAddress.getHost());
+		
+		future.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if (!future.isSuccess()) {
+                    ctx.channel().close();
+                }
+            }
+        });
 		
 		return future.channel();
 	}
-
-	private static class FullPath {
+	
+	private class ResolvedUri {
 		private String scheme;
-		private String host;
+		private String hostname;
 		private int port;
 		private String path;
-
-		private FullPath(String scheme, String host, int port, String path) {
+		
+		public ResolvedUri(String scheme, String hostname, int port, String path) {
 			this.scheme = scheme;
-			this.host = host;
+			this.hostname = hostname;
 			this.port = port;
 			this.path = path;
 		}
-
-		@Override
-		public String toString() {
-			return "FullPath{" + "scheme='" + scheme + '\'' + ", host='" + host + '\'' + ", port=" + port + ", path='"
-					+ path + '\'' + '}';
-		}
+		
+		
 	}
 }
